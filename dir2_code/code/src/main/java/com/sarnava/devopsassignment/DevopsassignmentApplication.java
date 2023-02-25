07528cd015f84ef493a400c6f79677923550c1ac
Sarnava Banerjee <sbsarnava@gmail.com>
2023-02-25 21:30:31 +0530

    Added two directories and put raw code into one

---
+++ dir2_code/code/src/main/java/com/sarnava/devopsassignment/DevopsassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sarnava.devopsassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsassignmentApplication.class, args);
+	}
+
+}
---
+++ dir2_code/code/src/test/java/com/sarnava/devopsassignment/DevopsassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarnava.devopsassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
